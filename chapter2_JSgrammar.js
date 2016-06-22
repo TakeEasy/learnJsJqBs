9b7ce4012ab5fbb67f56cf51ad5576cf9ab7a9f2
YEAR-PC <yearzhou93@gmail.com>
2016-06-23 07:54:09 +0800

    learn chapter2

---
+++ chapter2_JSgrammar.js
@@ -0,0 +1,13 @@
+/**
+ * Created by YEAR on 2016/6/21.
+ */
+var mood;
+var age;
+var temperature;
+var married;
+// mood = "happy";
+mood = "The mood is \"happy\"!!";
+age = 22;
+temperature = -34.66666;
+married = false;
+alert(mood);
