6b709fae145e9885bee57260aa8df8373f1c6226
YEAR <yearzhou93@gmail.com>
2016-06-29 17:21:45 +0800

    chapter4 go on

--- chapter4_imageGallery.js
+++ chapter4_imageGallery.js
@@ -6,3 +6,12 @@ function showPic(whichpic) {
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src",source);
 }
+function countBodyChild() {
+    var body_element = document.getElementsByTagName("body")[0];
+    alert(body_element.childNodes.length);
+    for(i=1;i<body_element.childNodes.length;i++){
+        alert(body_element.childNodes[i].getAttribute("id"))
+    }
+    
+}
+window.onload = countBodyChild();
