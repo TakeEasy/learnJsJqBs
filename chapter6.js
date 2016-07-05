d1b356bca35d507c61ff5146b6e9eb33d4fd4137
YEAR-PC <yearzhou93@gmail.com>
2016-07-05 20:10:12 +0800

    chapter 6

--- chapter6.js
+++ chapter6.js
@@ -2,6 +2,7 @@
  * Created by YEAR on 2016/6/28.
  */
 function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
     var text = whichpic.getAttribute("title");
     var placeholder = document.getElementById("placeholder");
@@ -23,5 +24,26 @@ function prepareGallery() {
     if (!document.getElementsByTagName) return false;
     if (!document.getElementById("imagegallery")) return false;
     var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for(var i = 0;i<links.length;i++) {
+        links[i].onclick = function () {
+            return !showPic(this);
+        }
+        // links[i].onkeypress = links[i].onclick;
+    }
 }
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+addLoadEvent(prepareGallery);
 // window.onload = countBodyChild();
