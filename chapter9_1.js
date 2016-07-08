bdc0a3bc19843a2defb3f4363be7ab385574ad19
YEAR <yearzhou93@gmail.com>
2016-07-08 16:12:12 +0800

    chapter 9

---
+++ chapter9_1.js
@@ -0,0 +1,68 @@
+/**
+ * Created by YEAR on 16/7/8.
+ */
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+function getNextElement(node) {
+    if(node.nodeType ==1){
+        return node;
+    }
+    if(node.nextSibling){
+        return getNextElement(node.nextSibling);
+    }
+    return null;
+}
+
+function styleHeaderSiblings() {
+    if (!document.getElementsByTagName) return false;
+    var header = document.getElementsByTagName("h1");
+    var elem;
+    for (var i = 0;i<header.length;i++){
+        elem = getNextElement(header[i].nextSibling);
+        elem.style.fontWeight = "bold";
+        elem.style.fontSize = "1.2em";
+    }
+}
+ function stripeTables() {
+     if(!document.getElementsByTagName) return false;
+     var tables = document.getElementsByTagName("table");
+     var odd, rows;
+     for (var i=0;i<tables.length;i++) {
+         odd = false;
+         rows = tables[i].getElementsByTagName("tr");
+         for (var j=0;j<rows.length;j++) {
+             if (odd == true) {
+                 rows[j].style.backgroundColor = "#ffc";
+                 odd = false;
+             } else {
+                 odd = true;
+             }
+         }
+     }
+ }
+
+function highlightRows() {
+    if(!document.getElementsByTagName) return false;
+    var rows = document.getElementsByTagName("tr");
+    for(var i = 0;i<rows.length;i++) {
+        rows[i].onmouseover = function () {
+            this.style.fontWeight = "bold";
+        }
+        rows[i].onmouseout = function () {
+            this.style.fontWeight = "normal";
+        }
+    }
+}
+addLoadEvent(styleHeaderSiblings);
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
