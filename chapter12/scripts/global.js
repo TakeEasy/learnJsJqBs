0cc15170789c063a935b6e285fc574d8d5c8cc27
YEAR <yearzhou93@gmail.com>
2016-07-14 15:45:59 +0800

    This book complete!!!!!

--- chapter12/scripts/global.js
+++ chapter12/scripts/global.js
@@ -169,6 +169,159 @@ function prepareInternalnav() {
     }
 }
 
+function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return true;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+    if (!document.getElementById("description")) return false;
+    if (whichpic.getAttribute("title")) {
+        var text = whichpic.getAttribute("title");
+    } else {
+        var text = "";
+    }
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType==3) {
+        description.firstChild.nodeValue = text;
+    }
+    return false;
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/placeholder.gif");
+    placeholder.setAttribute("alt","my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(description,gallery);
+    insertAfter(placeholder,description);
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i=0;i<links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this);
+        }
+    }
+}
+
+function stripeTables() {
+    if(!document.getElementsByTagName) return false;
+    var tables = document.getElementsByTagName("table");
+    var odd, rows;
+    for (var i=0;i<tables.length;i++) {
+        odd = false;
+        rows = tables[i].getElementsByTagName("tr");
+        for (var j=0;j<rows.length;j++) {
+            if (odd == true) {
+                // rows[j].style.backgroundColor = "#ffc";
+                addClass(rows[j],"odd");
+                odd = false;
+            } else {
+                odd = true;
+            }
+        }
+    }
+}
+
+function highlightRows() {
+    if(!document.getElementsByTagName) return false;
+    var rows = document.getElementsByTagName("tr");
+    for(var i = 0;i<rows.length;i++) {
+        rows[i].oldClassName = rows[i].className;
+        rows[i].onmouseover = function () {
+            addClass(this,"highlight");
+        }
+        rows[i].onmouseout = function () {
+            this.className = this.oldClassName;
+        }
+    }
+}
+
+function getHttPObject() {
+    if(typeof XMLHttpRequest == "undefined")
+        XMLHttpRequest = function () {
+            try { return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
+            catch (e) {}
+            try { return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
+            catch (e) {}
+            try { return new ActiveXObject("Msxml2.XMLHTTP");}
+            catch (e) {}
+            return false;
+        }
+    return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element) {
+    while (element.hasChildNodes()) {
+        element.removeChild(element.lastChild);
+    }
+    var content = document.createElement("img");
+    content.setAttribute("src","images/loading.gif");
+    content.setAttribute("alt","Loading...");
+    element.appendChild(content);
+}
+
+function submitFormWithAjax(whichform,thetarget) {
+    var request = getHttPObject();
+    if (!request) {return false;}
+    displayAjaxLoading(thetarget);
+    var dataParts = [];
+    var element;
+    for (var i=0;i<whichform.elements.length;i++) {
+        element = whichform.elements[i];
+        dataParts[i] = element.name + '=' + encodeURIComponent(element.value);
+    }
+    var data = dataParts.join('&');
+    request.open('POST',whichform.getAttribute("action"),true);
+    request.setRequestHeader("Contento-type", "apoplication/x-www-form-urlencoded");
+    request.onreadystatechange = function () {
+        if (request.readyState ==4){
+            if (request.status == 200 || request.status==0) {
+                var matches = request.responseText.match(/<article>([\s\S]+)<\/article>/);
+                if (matches.length>0){
+                    thetarget.innerHTML = matches[1];
+                } else {
+                    thetarget.innerHTML = '<p>Oops, there was an error. Sorry.</p>';
+                }
+            } else {
+                thetarget.innerHTML = '<p>' + request.statusText + '</p>';
+            }
+        }
+    };
+    request.send(data);
+    return true;
+}
+
+function prepareForms() {
+    for (var i=0;i<document.forms.length;i++) {
+        var thisform = document.forms[i];
+        thisform.onsubmit = function () {
+            var article = document.getElementsByTagName('article')[0];
+            if (submitFormWithAjax(this,article)) return false;
+            return true;
+        }
+    }
+}
+
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(prepareForms);
